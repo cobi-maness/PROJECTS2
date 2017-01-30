d56d91885a0e6a13b29375e2a1c95e92885aa8b1
Cobi <geekwise.cobi.maness@gmail.com>
2017-01-30 15:50:10 -0800

    started js

---
+++ jan-30/js/script.js
@@ -0,0 +1,19 @@
+// alert('I wanna die')
+var myNumberVar = 7;
+var myFloatVar = 5.2;
+var myStringVar = "Daddy Cobi";
+var myBooleanVar = true;
+var myArrayVar = ['one', 'stingray', 'banana', 8, true];
+var myObjectVar = {
+    make: "literally a trash can",
+    model: "the green one you put the green stuff in",
+    color: "green you dip",
+    year: "the plastic looks sorta new"
+};
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log(myObjectVar.model);
