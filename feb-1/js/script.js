8610ae810215e03d8de25569f2df9a2c478b9054
Cobi <geekwise.cobi.maness@gmail.com>
2017-02-01 15:55:17 -0800

    feb1

---
+++ feb-1/js/script.js
@@ -0,0 +1,25 @@
+var myNumberVar = 7;
+var raspberry = "monty";
+var strawberry = true;
+var orange = 6.9;
+var gus = ['is', 'not', 'a', 'good', 'friend' ];
+var mapple = {
+  bet: 'monty',
+  no: 'orange'
+};
+var dude = 8;
+var answer = dude + myNumberVar;
+var answer2 = dude / myNumberVar;
+var answer3 = dude * myNumberVar;
+var answer4 = dude - myNumberVar;
+
+console.log(typeof myNumberVar);
+console.log(typeof raspberry);
+console.log(typeof strawberry);
+console.log(typeof orange);
+console.log(typeof gus);
+console.log(typeof mapple);
+console.log(answer);
+console.log(answer2);
+console.log(answer3);
+console.log(answer4);
