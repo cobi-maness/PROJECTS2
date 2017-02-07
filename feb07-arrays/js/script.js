dcacf4f8b948d5d5b50cda0c4dd6498b846f154e
Cobi <geekwise.cobi.maness@gmail.com>
2017-02-07 15:54:25 -0800

    bet

--- feb07-arrays/js/script.js
+++ feb07-arrays/js/script.js
@@ -1,9 +1,54 @@
-var students = ['cobi', 'josh', 'monty', 'daddy', 'eric', 'gus', 'rosa', 'alfredo', 'leo', 'rigo', 'mikayla', 'osvaldo', 'david', 'matt', 'haley', 'melissa'];
-// console.log(students.length);
+// var students = ['cobi', 'josh', 'monty', 'daddy', 'eric', 'gus', 'rosa', 'alfredo', 'leo', 'rigo', 'mikayla', 'osvaldo', 'david', 'matt', 'haley', 'melissa'];
+// // console.log(students.length);
+// // function randomStudent(){
+// //   var numStudents = students.length;
+// //   var randStudent = Math.ceil( Math.random() * numStudents ) -1;
+// //   alert(students[randStudent]);
+// // }
+// // randomStudent();
+//
+//
+// var rappers = ['matty b raps', 'CXL', '3pac', 'Snoop Lion', 'The Beatles' , 'iced cube'];
+//
+// console.log(rappers);
+// rappers.push('childish gambleano');
+//
+// var lastRappers = rappers.pop();
+// console.log(rappers);
+//
+// rappers.unshift('headspace');
+// console.log(rappers);
+//
+// var firstRappers = rappers.shift();
+// console.log(rappers);
+//
+// var posRappers = rappers.indexOf('CXL');
+// console.log(posRappers);
+//
+// var remRappers = rappers.splice(1, 3);
+// console.log(remRappers);
+// console.log(rappers);
+//
+// var copyRappers = rappers.slice(2, 3);
+// // console.log(copyRappers);
+// // console.log(rappers);
 
-function randomStudent(){
-  var numStudents = students.length;
-  var randStudent = Math.ceil( Math.random() * numStudents ) -1;
-  alert(students[randStudent]);
-}
-randomStudent();
+
+
+var classes = ['spanish', 'math', 'geekwise', 'history', 'english'];
+console.log(classes);
+
+var nums = [0, 53, 5, 103, 7, 78, 10];
+// 0 5 10 53 78 103
+nums.sort();
+console.log(nums);
+//
+// classes.push('history', 'spanish');
+// console.log(classes);
+//
+// classes.unshift('math', 'geekwise');
+// console.log(classes);
+//
+// classes.pop(3);
+// console.log(classes);
+// display:flex;
